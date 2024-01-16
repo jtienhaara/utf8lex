e1b5fbe47fa1760aa19d94a0347a57f5d5e3264d
Johann Tienhaara <jtienhaara@yahoo.com>
2024-01-16 14:20:30 -0400

    Refactor multi-definition unit tests to make it easier to create new ones

--- tests/unit/test_utf8lex_definition_multi.c
+++ tests/unit/test_utf8lex_definition_multi.c
@@ -26,27 +26,45 @@ typedef struct _STRUCT_utf8lex_test_db
   utf8lex_definition_t *definitions_db;
   utf8lex_rule_t *rules_db;
 
+  // NUMBER
   utf8lex_cat_definition_t number_definition;
   utf8lex_rule_t number_rule;
 
+  // ID
   utf8lex_regex_definition_t id_definition;
   utf8lex_rule_t id_rule;
 
+  // EQUALS3
   utf8lex_literal_definition_t equals3_definition;
   utf8lex_rule_t equals3_rule;
 
+  // EQUALS
   utf8lex_literal_definition_t equals_definition;
   utf8lex_rule_t equals_rule;
 
+  // PLUS
   utf8lex_literal_definition_t plus_definition;
   utf8lex_rule_t plus_rule;
 
+  // MINUS
   utf8lex_literal_definition_t minus_definition;
   utf8lex_rule_t minus_rule;
 
+  // SPACE
   utf8lex_regex_definition_t space_definition;
   utf8lex_rule_t space_rule;
 } utf8lex_test_db_t;
+
+typedef struct _STRUCT_utf8lex_test_multi_operator
+{
+  // OPERATOR = EQUALS3 | EQUALS | PLUS | MINUS
+  utf8lex_multi_definition_t operator_definition;
+
+  utf8lex_reference_t ref_equals3;
+  utf8lex_reference_t ref_equals;
+  utf8lex_reference_t ref_plus;
+  utf8lex_reference_t ref_minus;
+} utf8lex_test_operator_t;
 
 
 static utf8lex_error_t test_utf8lex_create_db(
@@ -212,21 +230,22 @@ static utf8lex_error_t test_utf8lex_create_db(
 }
 
 
-static utf8lex_error_t test_utf8lex_multi_definitions(
-        utf8lex_test_db_t *db
+static utf8lex_error_t test_utf8lex_create_operator(
+        utf8lex_test_db_t *db,
+        utf8lex_test_operator_t *operator
         )
 {
-  if (db == NULL)
+  if (db == NULL
+      || operator == NULL)
   {
     return UTF8LEX_ERROR_NULL_POINTER;
   }
 
-  utf8lex_multi_definition_t multi;
   utf8lex_error_t error = UTF8LEX_OK;
 
   printf("  Building multi-definition 'operator':\n");  fflush(stdout);
   error = utf8lex_multi_definition_init(
-              &multi,  // self
+              &(operator->operator_definition),  // self
               (utf8lex_definition_t *) &(db->space_definition),  // prev
               "operator",  // name
               NULL,  // parent
@@ -235,62 +254,83 @@ static utf8lex_error_t test_utf8lex_multi_definitions(
 
   printf("    Adding EQUALS3 | EQUALS | PLUS | MINUS:\n");  fflush(stdout);
   utf8lex_reference_t *prev_ref = NULL;
-  utf8lex_reference_t ref_equals3;
   error = utf8lex_reference_init(
-              &ref_equals3,  // self
+              &(operator->ref_equals3),  // self
               prev_ref,  // prev
               db->equals3_definition.base.name,  // name
               1,  // min
               1,  // max
-              &multi);  // parent
+              &(operator->operator_definition));  // parent
   if (error != UTF8LEX_OK) { return error; }
-  prev_ref = &ref_equals3;
-  utf8lex_reference_t ref_equals;
+  prev_ref = &(operator->ref_equals3);
   error = utf8lex_reference_init(
-              &ref_equals,  // self
+              &(operator->ref_equals),  // self
               prev_ref,  // prev
               db->equals_definition.base.name,  // name
               1,  // min
               1,  // max
-              &multi);  // parent
+              &(operator->operator_definition));  // parent
   if (error != UTF8LEX_OK) { return error; }
-  prev_ref = &ref_equals;
-  utf8lex_reference_t ref_plus;
+  prev_ref = &(operator->ref_equals);
   error = utf8lex_reference_init(
-              &ref_plus,  // self
+              &(operator->ref_plus),  // self
               prev_ref,  // prev
               db->plus_definition.base.name,  // name
               1,  // min
               1,  // max
-              &multi);  // parent
+              &(operator->operator_definition));  // parent
   if (error != UTF8LEX_OK) { return error; }
-  prev_ref = &ref_plus;
-  utf8lex_reference_t ref_minus;
+  prev_ref = &(operator->ref_plus);
   error = utf8lex_reference_init(
-              &ref_minus,  // self
+              &(operator->ref_minus),  // self
               prev_ref,  // prev
               db->minus_definition.base.name,  // name
               1,  // min
               1,  // max
-              &multi);  // parent
+              &(operator->operator_definition));  // parent
   if (error != UTF8LEX_OK) { return error; }
-  prev_ref = &ref_minus;
+  prev_ref = &(operator->ref_minus);
 
+  // Just test resolving one of the references, to detect
+  // any bugs in reference-resolving before resolving
+  // the whole multi-definition:
   printf("  Resolving reference 'EQUALS3':\n");  fflush(stdout);
   error = utf8lex_reference_resolve(
-              &ref_equals3,  // self
+              &(operator->ref_equals3),  // self
               db->definitions_db);  // db
   if (error != UTF8LEX_OK) { return error; }
 
   printf("  Resolving multi-definition 'operator':\n");  fflush(stdout);
   error = utf8lex_multi_definition_resolve(
-              &multi,  // self
+              &(operator->operator_definition),  // self
               db->definitions_db);  // db
   if (error != UTF8LEX_OK) { return error; }
 
+  return UTF8LEX_OK;
+}
+
+
+static utf8lex_error_t test_utf8lex_multi_definitions()
+{
+  utf8lex_error_t error = UTF8LEX_OK;
+
+  utf8lex_test_db_t db;
+  error = test_utf8lex_create_db(&db);
+  if (error != UTF8LEX_OK)
+  {
+    return error;
+  }
+
+  utf8lex_test_operator_t operator;
+  error = test_utf8lex_create_operator(&db, &operator);
+  if (error != UTF8LEX_OK)
+  {
+    return error;
+  }
+
   printf("  Clearing multi-definition 'operator':\n");  fflush(stdout);
   error = utf8lex_multi_definition_clear(
-              (utf8lex_definition_t *) &multi);  // self
+              (utf8lex_definition_t *) &(operator.operator_definition));  // self
   if (error != UTF8LEX_OK) { return error; }
 
   return UTF8LEX_OK;
@@ -303,14 +343,8 @@ int main(
         )
 {
   printf("Testing utf8lex_definition_multi...\n");  fflush(stdout);
-  utf8lex_test_db_t db;
-  utf8lex_error_t error = test_utf8lex_create_db(&db);
 
-  if (error == UTF8LEX_OK)
-  {
-    error = test_utf8lex_multi_definitions(&db);
-  }
-  // else fall through to print errors.
+  utf8lex_error_t error = test_utf8lex_multi_definitions();
 
   if (error == UTF8LEX_OK)
   {
